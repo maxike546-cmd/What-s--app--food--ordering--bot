4ce9d73e26022a837cd3f46569555883d5b78f5d
maxike546-cmd <maxike546@gmail.com>
2026-08-27 23:23:03 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -32,9 +32,9 @@ app.post("/whatsapp", (req, res) => {
   const pricePerItem = menu[item as keyof typeof menu] || 0;
   const total = pricePerItem * quantity;
   if (isNaN(total)) {
-    res.send("<rSorry, I don't understand. Try something like '2 rice'.");
+    res.send("<Response><Message>Sorry, I don't understand. Try something like '2 rice'</Message></Response>");
   } else {
-    res.send("You ordered " + quantity + "x" + item + ". Total: NGN" + total);
+    res.send("<Response><Message>You ordered " + quantity + "x" + item + ". Total: NGN" + total+"</Response></Message>");
   }
 });
 
